e6cc58b2d3f736e2bf59eb6a6e90af38196e6636
Philémon van Helden <philemon.vanhelden@gmx.com>
2022-07-05 22:00:40 +0200

    [ADD] Kotlin Multiplatform App Starter

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Shit_Idle"
+include(":androidApp")
+include(":shared")
---
+++ shared/src/commonMain/kotlin/com/example/shitidle/Platform.kt
@@ -0,0 +1,5 @@
+package com.example.shitidle
+
+expect class Platform() {
+    val platform: String
+}
